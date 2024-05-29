edbb0518a2a0c2e4003921a280918466f18f763b
Lorenz <lorenzrenti@outlook.com>
2024-05-29 15:15:49 +0200

    Second Failing Test - Rentenberger

--- spec/electrons_around_the_core_spec.js
+++ spec/electrons_around_the_core_spec.js
@@ -6,4 +6,8 @@ describe('Electrons around the cores', function() {
 	it('should return "Parameter has to be array" for a parameter that is not an array', function() {
 		expect(findElectrons.add(1)).toEqual("Parameter has to be array");
 	});
+
+	it('should return "Array too short" for a parameter that is shorter than 4', function() {
+		expect(findElectrons.add([5, 3, 5])).toEqual("Array too short");
+	});
 });
