78da5a501bb50b8d7cc6cef4242108a2a8743aee
samuelhammerschmidt <if23b042@technikum-wien.at>
2024-05-29 15:31:27 +0200

    4 Production Code - Hammerschmidt

--- src/electrons_around_the_cores.js
+++ src/electrons_around_the_cores.js
@@ -9,5 +9,7 @@ electrons_around_the_cores.prototype.add = function(numbers) {
 		return "Array too short";
 	} else if (numbers.length > 6) {
 		return "Array too long";
+	} else {
+		return 0;
 	}
 };
