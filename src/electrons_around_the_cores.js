767aca0ddee1ec9edb6f73d2684dc8c15a2e0bc3
samuelhammerschmidt <if23b042@technikum-wien.at>
2024-05-29 15:18:10 +0200

    Second Production Code - Hammerschmidt

--- src/electrons_around_the_cores.js
+++ src/electrons_around_the_cores.js
@@ -4,4 +4,8 @@ electrons_around_the_cores.prototype.add = function(numbers) {
 	if (!Array.isArray(numbers)) {
 		return "Parameter has to be array";
 	}
+
+	if (numbers.length < 4) {
+		return "Array too short";
+	}
 };
