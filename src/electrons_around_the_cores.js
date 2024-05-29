7101b459e683f1d1f24e7a57a3bb79f0c8119d8b
samuelhammerschmidt <if23b042@technikum-wien.at>
2024-05-29 16:07:49 +0200

    Final Refactoring - Hammerschmidt

--- src/electrons_around_the_cores.js
+++ src/electrons_around_the_cores.js
@@ -1,25 +1,29 @@
 // ElectronsAroundTheCores.js
 function electrons_around_the_cores() {}
 electrons_around_the_cores.prototype.add = function(numbers) {
+
 	if (!Array.isArray(numbers)) {
 		return "Parameter has to be array";
 	}
 
+	var sum = 0;
+
 	if (numbers.length < 4) {
 		return "Array too short";
-	} else if (numbers.length > 6) {
-		return "Array too long";
-	} else {
-		let sum = 0;
-		for (var i = 0; i < numbers.length; i++) {
-			if (numbers[i] < 1 || numbers[i] > 6) {
-				return "Invalid number in array";
-			}
-			
-			if (numbers[i] % 2 != 0) {
-				sum += (numbers[i] - 1);
-			}
-		}
-		return sum;
 	}
+	else if (numbers.length > 6) {
+		return "Array too long";
+	}
+
+	for (var i = 0; i < numbers.length; i++) {
+		if (numbers[i] < 1 || numbers[i] > 6) {
+			return "Invalid number in array";
+		}
+			
+		if (numbers[i] % 2 != 0) {
+			sum += (numbers[i] - 1);
+		}
+	}
+	
+	return sum;
 };
