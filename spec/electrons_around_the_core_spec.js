bb81b8bc376086a8c1094fb5788628aeca15b7a8
samuelhammerschmidt <if23b042@technikum-wien.at>
2024-05-29 14:00:26 +0200

    Initial structure

---
+++ spec/electrons_around_the_core_spec.js
@@ -0,0 +1,8 @@
+describe('Electrons around the cores', function() {
+	beforeEach(function() {
+		findElectrons = new ElectronsAroundTheCore();
+
+		// TODO: Add test cases
+
+	});
+});
---
+++ src/electrons_around_the_cores.js
@@ -0,0 +1,5 @@
+// ElectronsAroundTheCores.js
+function electrons_around_the_cores() {}
+electrons_around_the_cores.add = function(numbers) {
+	// TODO: Add implementation	
+};
